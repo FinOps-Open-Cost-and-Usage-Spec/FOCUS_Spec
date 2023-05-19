47118cde7227477cf2c523487f2ccb53df0c294c
Mike Fuller <mike@finops.org>
2023-05-19 11:59:56 +1000

    Issue 68 Add custom filter for smart/curly quotes
    Added new custom pymarkdownlnt plugin to find smart quotes
    Added path to the custom plugin to the config for pymarkdownlnt

---
+++ custom_linter_rules/rule_md_990.py
@@ -0,0 +1,75 @@
+"""
+Module to implement a plugin that looks for smart/curly quotes in the files.
+"""
+from typing import cast
+
+from pymarkdown.inline_markdown_token import TextMarkdownToken
+from pymarkdown.markdown_token import MarkdownToken
+from pymarkdown.parser_helper import ParserHelper
+from pymarkdown.plugin_manager.plugin_details import PluginDetails
+from pymarkdown.plugin_manager.plugin_scan_context import PluginScanContext
+from pymarkdown.plugin_manager.rule_plugin import RulePlugin
+
+
+class RuleMd990(RulePlugin):
+    """
+    Class to implement a plugin that looks for smart quotes in the files.
+    """
+    __smart_quote_items = ['“', '”']
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def get_details(self) -> PluginDetails:
+        """
+        Get the details for the plugin.
+        """
+        return PluginDetails(
+            plugin_name="no-smart-quotes",
+            plugin_id="MD990",
+            plugin_enabled_by_default=True,
+            plugin_description="Smart/curly quote found",
+            plugin_version="0.1.0",
+            plugin_interface_version=1,
+            plugin_url="",
+        )
+
+    def next_token(self, context: PluginScanContext, token: MarkdownToken) -> None:
+        """
+        Event that a new token is being processed.
+        """
+        if (
+            token.is_text
+        ):
+            text_token = cast(TextMarkdownToken, token)
+            for smart_quote_item in RuleMd990.__smart_quote_items:
+                start_index = 0
+                found_index = text_token.token_text.find(smart_quote_item, start_index)
+                while found_index != -1:
+                    (
+                        column_number_delta,
+                        line_number_delta,
+                    ) = ParserHelper.adjust_for_newlines(text_token.token_text, 0, found_index)
+                    self.report_next_token_error(
+                        context,
+                        token,
+                        line_number_delta=line_number_delta,
+                        column_number_delta=column_number_delta,
+                    )
+                    start_index = found_index + len(smart_quote_item)
+                    found_index = text_token.token_text.find(smart_quote_item, start_index)
+
+
+        elif token.is_code_block:
+            self.__in_code_block = True
+        elif token.is_code_block_end:
+            self.__in_code_block = False
+        elif token.is_html_block:
+            self.__in_html_block = True
+        elif token.is_html_block_end:
+            self.__in_html_block = False
+        elif token.is_inline_link:
+            self.__in_link = True
+        elif token.is_inline_link_end:
+            self.__in_link = False
+
