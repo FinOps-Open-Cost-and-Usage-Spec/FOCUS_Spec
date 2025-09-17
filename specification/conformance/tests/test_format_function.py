b98a1557d16a78ad7920f7192deec83da5d02b49
Mike Fuller <mike@finops.org>
2025-09-17 17:39:51 +1000

    Manually imported the CR contributions my mflanakin (ContractedCost) Added tests for more consistency uses and applied fixes Signed-off-by: Mike Fuller <mike@finops.org>

---
+++ specification/conformance/tests/test_format_function.py
@@ -0,0 +1,23 @@
+def test_mustsatisfy_format_requires_function_format(cr_json):
+    rules = cr_json.get("ConformanceRules") or {}
+    violations = []
+
+    for rid, rule in rules.items():
+        func = rule.get("Function")
+        if func == "Composite":
+            continue  # skip composite rules
+
+        vc = rule.get("ValidationCriteria") or {}
+        mustsatisfy = vc.get("MustSatisfy")
+
+        if isinstance(mustsatisfy, str) and "format" in mustsatisfy.lower():
+            if func != "Format":
+                violations.append((rid, func, mustsatisfy))
+
+    assert not violations, (
+        "Any non-Composite rule whose MustSatisfy contains 'format' must have Function='Format':\n"
+        + "\n".join(
+            f"- Rule {rid}: Function='{func}', MustSatisfy='{ms}'"
+            for rid, func, ms in violations
+        )
+    )
