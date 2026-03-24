d1c93963db5258192b7c1062320fd0489ea14e79
Mike Fuller <mike@finops.org>
2026-03-25 09:59:05 +1100

    Updated wkhtmltopdf to latest version Cleaned up pymarkdownlnt rule to support version 0.9.36 imports Signed-off-by: Mike Fuller <mike@finops.org>

--- custom_linter_rules/rule_md_990.py
+++ custom_linter_rules/rule_md_990.py
@@ -3,12 +3,11 @@ Module to implement a plugin that looks for smart characters in the files.
 """
 from typing import cast
 
-from pymarkdown.inline_markdown_token import TextMarkdownToken
-from pymarkdown.markdown_token import MarkdownToken
-from pymarkdown.parser_helper import ParserHelper
+from pymarkdown.tokens.inline_markdown_token import InlineMarkdownToken
+from pymarkdown.tokens.markdown_token import MarkdownToken
+from pymarkdown.general.parser_helper import ParserHelper
 from pymarkdown.plugin_manager.plugin_details import PluginDetails
-from pymarkdown.plugin_manager.plugin_scan_context import PluginScanContext
-from pymarkdown.plugin_manager.rule_plugin import RulePlugin
+from pymarkdown.plugin_manager.rule_plugin import PluginScanContext, RulePlugin
 
 
 class RuleMd990(RulePlugin):
@@ -41,7 +40,7 @@ class RuleMd990(RulePlugin):
         if (
             token.is_text
         ):
-            text_token = cast(TextMarkdownToken, token)
+            text_token = cast(InlineMarkdownToken, token)
             for smart_char_item in RuleMd990.__smart_char_items:
                 start_index = 0
                 found_index = text_token.token_text.find(smart_char_item, start_index)
@@ -59,17 +58,4 @@ class RuleMd990(RulePlugin):
                     start_index = found_index + len(smart_char_item)
                     found_index = text_token.token_text.find(smart_char_item, start_index)
 
-
-        elif token.is_code_block:
-            self.__in_code_block = True
-        elif token.is_code_block_end:
-            self.__in_code_block = False
-        elif token.is_html_block:
-            self.__in_html_block = True
-        elif token.is_html_block_end:
-            self.__in_html_block = False
-        elif token.is_inline_link:
-            self.__in_link = True
-        elif token.is_inline_link_end:
-            self.__in_link = False
 
