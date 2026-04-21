7403c6e162ffd3e1e415ae55af51d6382e396dae
rileyjenk <rileyjenk@gmail.com>
2026-04-21 12:39:58 -0600

    Update custom_linter_rules/tests/test_title_case.py
    Co-authored-by: Shawn Alpay <shawn@finops.org>

--- custom_linter_rules/tests/test_title_case.py
+++ custom_linter_rules/tests/test_title_case.py
@@ -6,8 +6,9 @@ from rule_md_991 import RuleMd991
 plugin = RuleMd991()
 
 # Test the _is_title_case method
+# Test the _is_title_case method
 test_cases = [
-    ("This Is a Test Heading", True),
+    ("This Is a Test Heading", False), # 'is' should be lowercase now
     ("this is a test heading", False),
     ("This is a Test Heading", True),  # 'is' and 'a' should be lowercase
     ("Another heading with bad case", False),  # 'heading' should be capitalized
